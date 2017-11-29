2824c8cd4127e464b147760278c56cb2cffd8d55
original_ar <aaroach@gmail.com>
2017-11-28 21:58:38 -0600

    Updates

--- private/query_functions.php
+++ private/query_functions.php
@@ -30,7 +30,7 @@ function insert_subject($subject) {
   $sql .= "VALUES (";
   $sql .= "'" . $subject['menu_name'] . "',";
   $sql .= "'" . $subject['position'] . "',";
-  $sql .= "'" . $$subject['visible'] . "'";
+  $sql .= "'" . $subject['visible'] . "'";
   $sql .= ")";
   $result = mysqli_query($db, $sql);
   // For INSERT statements, $result is true/false
@@ -45,7 +45,7 @@ function insert_subject($subject) {
   }
 }
 
-function update_subject ($subject) {
+function update_subject($subject) {
   global $db;
 
   $sql = "UPDATE subjects SET ";
@@ -68,6 +68,24 @@ function update_subject ($subject) {
   }
 }
 
+function delete_subject($id) {
+  global $db;
+  $sql = "DELETE FROM subjects ";
+  $sql .= "WHERE id='" . $id . "' ";
+  $sql .= "LIMIT 1";
+  $result = mysqli_query($db, $sql);
+
+  //For DELETE statements, $result is true/find_all_subjects
+  if($result) {
+  return true;
+  } else {
+  // DELETE failed
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;
+  }
+}
+
 function find_all_pages() {
   global $db;
 
@@ -76,6 +94,89 @@ function find_all_pages() {
   $result = mysqli_query($db, $sql);
   confirm_result_set($result);
   return $result;
+}
+
+function find_page_by_id($id) {
+  global $db;
+
+  $sql = "SELECT * FROM pages ";
+  $sql .="WHERE id='" . $id . "'";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  $page = mysqli_fetch_assoc($result);
+  mysqli_free_result($result);
+  return $page; //returns an assoc. array
+}
+
+function insert_page($page) {
+  global $db;
+
+  $sql = "INSERT INTO pages ";
+  $sql .= "(fk_subject_id, menu_name, position, visible, content) ";
+  $sql .= "VALUES (";
+  $sql .= "'" . $page['fk_subject_id'] . "',";
+  $sql .= "'" . $page['menu_name'] . "',";
+  $sql .= "'" . $page['position'] . "',";
+  $sql .= "'" . $page['visible'] . "',";
+  $sql .= "'" . $page['content'] . "'";
+  $sql .= ")";
+  $result = mysqli_query($db, $sql);
+  // For INSERT statements, $result is true/false
+      if($result) {
+        return true;
+      } else {
+        // INSERT failed
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+      }
+
+
+
+}
+
+function update_page($page) {
+  global $db;
+
+  $sql = "UPDATE pages SET ";
+  $sql .= "fk_subject_id='" . $page['fk_subject_id'] . "', ";
+  $sql .= "menu_name='" . $page['menu_name'] . "', ";
+  $sql .= "position='" . $page['position'] . "', ";
+  $sql .= "visible='" . $page['visible'] . "', ";
+  $sql .= "content='" . $page['content'] . "' ";
+  $sql .= "WHERE id='" . $page['id'] . "' ";
+  $sql .= "LIMIT 1";
+
+  $result = mysqli_query($db, $sql);
+  // For UPDATE statements, $result is true/false
+  if($result) {
+    return true;
+      } else {
+        // UPDATE failed
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+      }
+
+
+}
+
+function delete_page($id) {
+  global $db;
+  $sql = "DELETE FROM pages ";
+  $sql .= "WHERE id='" . $id . "' ";
+  $sql .= "LIMIT 1";
+  $result = mysqli_query($db, $sql);
+
+  //For DELETE statements, $result is true/find_all_subjects
+  if($result) {
+  return true;
+  } else {
+  // DELETE failed
+  echo mysqli_error($db);
+  db_disconnect($db);
+  exit;
+  }
 }
 
 ?>
--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -4,35 +4,50 @@ require_once ('../../../private/initialize.php');
 
 $id = $_GET['id'] ?? '1';
 
+$page = find_page_by_id($id);
 
-
-$page_title = 'Show Subject';
-
-include(SHARED_PATH . '/staff_header.php');
 
 ?>
 
-<?php
-
-$page_title = 'Show'; ?>
-
-<HTML>
-  <BODY>
-    <a href = "index.php"><< Back to List</a>
-    <BR><BR>
+<?php $subject = find_subject_by_id($page['fk_subject_id']); ?>
 
 <?php
-
-echo "Page id: " . h($id);
-
+$page_title = 'Show Page';
+include(SHARED_PATH . '/staff_header.php');
+require_once ('../../../private/initialize.php');
+$id = $_GET['id'] ?? '1';
 ?>
 
 <div id='content'>
 
+
+<a href = "index.php"><< Back to List</a>
+<BR><BR>
+
+  <h1>Page: <?php echo h($page['menu_name']); ?></h1>
+
+  <div class="attributes">
+    <dl>
+      <dt>Subject Name</dt>
+      <dd><?php echo h($subject['menu_name']); ?></dd>
+    </dl>
+    <dl>
+      <dt>Menu Name</dt>
+      <dd><?php echo h($page['menu_name']); ?></dd>
+    </dl>
+    <dl>
+      <dt>Position</dt>
+      <dd><?php echo h($page['position']); ?></dd>
+    </dl>
+    <dl>
+      <dt>Visible</dt>
+      <dd><?php echo $page['visible'] == '1' ? 'true' : 'false'; ?></dd>
+    </dl>
+    <dl>
+      <dt>Content</dt>
+      <dd><?php echo h($page['content']); ?></dd>
+    </dl>
+  </div>
 </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
-
-
-</BODY>
-</HTML>
